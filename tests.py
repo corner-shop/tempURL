7da084fb97dea1399dee117239ee9ed3f3517ccb
Azul <mail@azulinho.com>
2016-06-25 22:36:14 +0100

    tests.py: support new UX

--- tests.py
+++ tests.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python -tt -u
 
-from hypothesis import given
-from hypothesis.strategies import text
+from hypothesis import (given, example)
+from hypothesis.strategies import (text, integers)
 import unittest
 import os
 import time
 import requests
 import json
+import StringIO
+import re
 
 class TestStringMethods(unittest.TestCase):
     @classmethod
@@ -20,38 +22,93 @@ class TestStringMethods(unittest.TestCase):
     def tearDownClass(cls):
         os.system('docker-compose down')
 
-    @given(text(min_size=1))
-    def test_not_null_upload_returns_200(self, s):
-        payload={'tempurl': s,
-                 'data': s}
+    @given(text(min_size=0), integers(min_value=300), text(min_size=0))
+    @example('',1,'')
+    @example(0,1,0)
+    @example('000000',1,'000000')
+    def test_post(self, tempurl, ttl, data):
 
-        r = requests.post('http://172.17.0.1:6222/upload',json=payload)
-        assert r.status_code == 200
+        f = StringIO.StringIO(data)
+        files = {'file': f }
+        url = 'http://172.17.0.1:6222/api'
+        values = {'tempurl': tempurl, 'ttl': ttl}
 
-    @given(text(min_size=0, max_size=0))
-    def test_null_upload_returns_415(self, s):
-        payload={'tempurl': s,
-                 'data': s}
+        r = requests.post(url, files=files, params=values)
 
-        r = requests.post('http://172.17.0.1:6222/upload',json=payload)
-        assert r.status_code == 415
+        # code should not accept an int as an url
+        if not isinstance(tempurl, basestring):
+            assert r.status_code == 404
+        else:
+            # code should not accept an empty string as an url
+            if len(tempurl) == 0:
+                assert r.status_code == 404
+            else:
+                # hypothesis will generate invalid strings for the tempurl
+                # we expect the code to reject them with a 404.
+                if not re.match("^[A-Za-z0-9_-]{4,}$", str(tempurl.encode('utf-8'))):
+                    assert r.status_code == 404
+                else:
+                    assert r.status_code == 200
 
-    @given(text(min_size=1))
-    def test_not_null_download_returns_200(self, s):
-        payload={'tempurl': s,
-                 'data': s}
+    @given(text(min_size=0), integers(min_value=300), text(min_size=0))
+    @example('',1,'')
+    @example(0,1,0)
+    @example('000000',1,'000000')
+    def test_get_matches_upload(self, tempurl, ttl, data):
 
-        r = requests.post('http://172.17.0.1:6222/upload',json=payload)
-        payload={'tempurl': s}
-        r = requests.get('http://172.17.0.1:6222/download',json=payload)
-        print r.status_code
-        assert r.status_code == 200
+        # upload random file
+        f = StringIO.StringIO(data)
+        files = {'file': f }
+        url = 'http://172.17.0.1:6222/api'
+        values = {'tempurl': tempurl, 'ttl': ttl}
 
-    @given(text(min_size=0, max_size=0))
-    def test_null_download_returns_415(self, s):
-        payload={'tempurl': s}
-        r = requests.get('http://172.17.0.1:6222/download',json=payload)
-        assert r.status_code == 415
+        r = requests.post(url, files=files, params=values)
+
+        # download uploaded file
+        values = { 'tempurl': tempurl }
+
+        r = requests.get(url, params=values)
+
+        # code should not accept an int as an url
+        if not isinstance(tempurl, basestring):
+            assert r.status_code == 404
+        else:
+            # code should not accept an empty string as an url
+            if len(tempurl) == 0:
+                assert r.status_code == 404
+            else:
+                # hypothesis will generate invalid strings for the tempurl
+                # we expect the code to reject them with a 404.
+                if not re.match("^[A-Za-z0-9_-]{4,}$", str(tempurl.encode('utf-8'))):
+                    assert r.status_code == 404
+                else:
+                    # expect a successull download
+                    assert r.status_code == 200
+                    # and that our download actually matches our upload
+                    assert r.content == data
+                    # a second download request should return a 404
+                    r = requests.get(url, params=values)
+                    assert r.status_code == 404
+
+    def test_get_expires_after_ttl(self):
+        tempurl = 'kjasdkfasdf'
+        data = 'adsfadsf'
+        ttl = 1
+
+        # upload random file
+        f = StringIO.StringIO(data)
+        files = {'file': f }
+        url = 'http://172.17.0.1:6222/api'
+        values = {'tempurl': tempurl, 'ttl': ttl}
+
+        r = requests.post(url, files=files, params=values)
+
+        # download uploaded file
+        values = { 'tempurl': tempurl }
+
+        time.sleep(2)
+        r = requests.get(url, params=values)
+        assert r.status_code == 404
 
 
 if __name__ == '__main__':
